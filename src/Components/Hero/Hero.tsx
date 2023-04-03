2cf47148cca5dacfcc50fb9e79885eb484b9c233
xroobix <109462535+xroobix@users.noreply.github.com>
2023-04-03 22:56:52 +0200

    Add id and change desc of Hero comp

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import darkThemeHeroImg from "../../images/hero-img.png"
 
 export const Hero = () => {
   return (
-    <section className="hero__section">
+    <section className="hero__section" id='home'>
       <div className="container">
         <div className="hero__wrapper">
           <div className="hero__content">
@@ -12,7 +12,7 @@ export const Hero = () => {
               <h2>Digital Products To</h2>
               <h2 className="highlight">Promote Your Brand</h2>
             </div>
-            <p className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <p className="description">FutureMedia is a digital marketing agency that helps businesses grow online through services like SEO, social media marketing, and PPC advertising. We're dedicated to innovation and delivering results-driven strategies for our clients.</p>
             <div className="hero__btns">
                 <button className="primary__btn">Get Started Now</button>
                 <button className="secondary__btn">Discover More</button>
