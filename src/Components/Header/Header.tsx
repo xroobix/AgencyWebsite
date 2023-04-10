124ce7d87fcf58b0cfb383e0f07f9d6af713604b
Kacper Robak <kacper.roob.robak@gmail.com>
2023-04-10 20:05:18 +0200

    Add light mode and typesafety Function changeStickiness check for user postion on website. Base on position it add or remove header class with positon sticky. useEffect listen for this position when user scrolls. Function handleLinkClick prevents href from refreshing website and remove height of sticky navbar from scrollTo position.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,55 +1,127 @@
+import { FC, useEffect, useRef } from 'react';
 import './header.css';
 
-export const Header = () => {
-  const nav__links = [
-    {
-      path: '#home',
-      display: 'Home',
-    },
-    {
-      path: '#projects',
-      display: 'Projects',
-    },
-    {
-      path: '#services',
-      display: 'Services',
-    },
-    {
-      path: '#about',
-      display: 'About',
-    },
-    {
-      path: '#blog',
-      display: 'Blog',
-    },
-  ];
+interface Props {
+    theme: string;
+    changeTheme: () => void;
+}
 
-  return (
-    <header className="header">
-      <div className="container">
-        <div className="nav__wrapper">
-          <div className="logo">
-            <h2><a href="#">FutureMedia</a></h2>
-          </div>
-          <div className="navigation">
-            <ul className="menu">
-              {nav__links.map((i, idx) => (
-                <li key={idx} className="menu__item">
-                  <a href={i.path} className="menu__link">
-                    {i.display}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="light__mode">
-            <button>
-              <i className="ri-sun-line" />
-              Light Mode
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+export const Header: FC<Props> = ({ theme, changeTheme }) => {
+    const nav__links = [
+        {
+            path: '#home',
+            display: 'Home',
+        },
+        {
+            path: '#projects',
+            display: 'Projects',
+        },
+        {
+            path: '#services',
+            display: 'Services',
+        },
+        {
+            path: '#about',
+            display: 'About',
+        },
+        {
+            path: '#blog',
+            display: 'Blog',
+        },
+    ];
+
+    const darkMode = (
+        <>
+            <i className="ri-sun-line" />
+            <span>Light Mode</span>
+        </>
+    );
+
+    const lightMode = (
+        <>
+            <i className="ri-moon-line" />
+            <span>Dark Mode</span>
+        </>
+    );
+
+    const headerRef = useRef<HTMLElement>(null);
+
+    const changeStickiness = () => {
+        if (
+            document.body.scrollTop > 80 ||
+            document.documentElement.scrollTop > 80
+        ) {
+            headerRef.current &&
+                headerRef.current.classList.add('header__shrink');
+        } else {
+            headerRef.current &&
+                headerRef.current.classList.remove('header__shrink');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeStickiness);
+
+        return () =>
+            window.removeEventListener('scroll', changeStickiness);
+    }, []);
+
+    const handleLinkClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+
+        const target = e.target as HTMLAnchorElement;
+
+        const targetAttribute = target.getAttribute('href');
+
+        if (targetAttribute !== null) {
+            const element = document.querySelector(
+                targetAttribute
+            ) as HTMLElement;
+
+            if (element !== null) {
+                const location = element.offsetTop;
+
+                window.scrollTo({
+                    left: 0,
+                    top: location - 80,
+                });
+            }
+        }
+    };
+
+    return (
+        <header className="header" ref={headerRef}>
+            <div className="container">
+                <div className="nav__wrapper">
+                    <div className="logo">
+                        <h2>
+                            <a href="#">FutureMedia</a>
+                        </h2>
+                    </div>
+                    <div className="navigation">
+                        <ul className="menu">
+                            {nav__links.map((i, idx) => (
+                                <li key={idx} className="menu__item">
+                                    <a
+                                        href={i.path}
+                                        className="menu__link"
+                                        onClick={handleLinkClick}
+                                    >
+                                        {i.display}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="light__mode">
+                        <button onClick={changeTheme}>
+                            {theme === 'light-theme'
+                                ? lightMode
+                                : darkMode}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
