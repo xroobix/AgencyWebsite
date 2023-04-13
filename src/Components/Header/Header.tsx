f704bbfa54fc905f8ffd78a4c8a58eaaf54ea0c3
xroobix <109462535+xroobix@users.noreply.github.com>
2023-04-13 23:01:04 +0200

    Add hamburger mobile menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,126 +2,138 @@ import { FC, useEffect, useRef } from 'react';
 import './header.css';
 
 interface Props {
-    theme: string;
-    changeTheme: () => void;
+  theme: string;
+  changeTheme: () => void;
 }
 
 export const Header: FC<Props> = ({ theme, changeTheme }) => {
-    const nav__links = [
-        {
-            path: '#home',
-            display: 'Home',
-        },
-        {
-            path: '#projects',
-            display: 'Projects',
-        },
-        {
-            path: '#services',
-            display: 'Services',
-        },
-        {
-            path: '#about',
-            display: 'About',
-        },
-        {
-            path: '#blog',
-            display: 'Blog',
-        },
-    ];
+  const nav__links = [
+    {
+      path: '#home',
+      display: 'Home',
+    },
+    {
+      path: '#projects',
+      display: 'Projects',
+    },
+    {
+      path: '#services',
+      display: 'Services',
+    },
+    {
+      path: '#about',
+      display: 'About',
+    },
+    {
+      path: '#blog',
+      display: 'Blog',
+    },
+  ];
 
-    const darkMode = (
-        <>
-            <i className="ri-sun-line" />
-            <span>Light Mode</span>
-        </>
-    );
+  const darkMode = (
+    <>
+      <i className="ri-sun-line" />
+      <span>Light Mode</span>
+    </>
+  );
 
-    const lightMode = (
-        <>
-            <i className="ri-moon-line" />
-            <span>Dark Mode</span>
-        </>
-    );
+  const lightMode = (
+    <>
+      <i className="ri-moon-line" />
+      <span>Dark Mode</span>
+    </>
+  );
 
-    const headerRef = useRef<HTMLElement>(null);
+  const headerRef = useRef<HTMLDivElement>(null);
 
-    const changeStickiness = () => {
-        if (
-            document.body.scrollTop > 80 ||
-            document.documentElement.scrollTop > 80
-        ) {
-            headerRef.current &&
-                headerRef.current.classList.add('header__shrink');
-        } else {
-            headerRef.current &&
-                headerRef.current.classList.remove('header__shrink');
-        }
-    };
+  const menuRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        window.addEventListener('scroll', changeStickiness);
+  const changeStickiness = () => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      headerRef.current &&
+        headerRef.current.classList.add('header__shrink');
+    } else {
+      headerRef.current &&
+        headerRef.current.classList.remove('header__shrink');
+    }
+  };
 
-        return () =>
-            window.removeEventListener('scroll', changeStickiness);
-    }, []);
+  useEffect(() => {
+    window.addEventListener('scroll', changeStickiness);
 
-    const handleLinkClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
+    return () =>
+      window.removeEventListener('scroll', changeStickiness);
+  }, []);
 
-        const target = e.target as HTMLAnchorElement;
+  const handleLinkClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
 
-        const targetAttribute = target.getAttribute('href');
+    const target = e.target as HTMLAnchorElement;
 
-        if (targetAttribute !== null) {
-            const element = document.querySelector(
-                targetAttribute
-            ) as HTMLElement;
+    const targetAttribute = target.getAttribute('href');
 
-            if (element !== null) {
-                const location = element.offsetTop;
+    if (targetAttribute !== null) {
+      const element = document.querySelector(
+        targetAttribute
+      ) as HTMLElement;
 
-                window.scrollTo({
-                    left: 0,
-                    top: location - 80,
-                });
-            }
-        }
-    };
+      if (element !== null) {
+        const location = element.offsetTop;
 
-    return (
-        <header className="header" ref={headerRef}>
-            <div className="container">
-                <div className="nav__wrapper">
-                    <div className="logo">
-                        <h2>
-                            <a href="#">FutureMedia</a>
-                        </h2>
-                    </div>
-                    <div className="navigation">
-                        <ul className="menu">
-                            {nav__links.map((i, idx) => (
-                                <li key={idx} className="menu__item">
-                                    <a
-                                        href={i.path}
-                                        className="menu__link"
-                                        onClick={handleLinkClick}
-                                    >
-                                        {i.display}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="light__mode">
-                        <button onClick={changeTheme}>
-                            {theme === 'light-theme'
-                                ? lightMode
-                                : darkMode}
-                        </button>
-                    </div>
-                </div>
+        window.scrollTo({
+          left: 0,
+          top: location - 80,
+        });
+      }
+    }
+  };
+
+  const toggleMobileMenu = () =>
+    menuRef.current?.classList.toggle('menu__active');
+
+  return (
+    <header className="header" ref={headerRef}>
+      <div className="container">
+        <div className="nav__wrapper">
+          <div className="logo">
+            <h2>
+              <a href="#">FutureMedia</a>
+            </h2>
+          </div>
+          <div
+            className="navigation"
+            ref={menuRef}
+            onClick={toggleMobileMenu}
+          >
+            <ul className="menu">
+              {nav__links.map((i, idx) => (
+                <li key={idx} className="menu__item">
+                  <a
+                    href={i.path}
+                    className="menu__link"
+                    onClick={handleLinkClick}
+                  >
+                    {i.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='right-menu'>
+            <div className="light__mode">
+              <button onClick={changeTheme}>
+                {theme === 'light-theme' ? lightMode : darkMode}
+              </button>
             </div>
-        </header>
-    );
+            <span className="mobile__menu" onClick={toggleMobileMenu}>
+              <i className="ri-menu-line" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
