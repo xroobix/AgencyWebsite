78c0f9a5ac643c153b95324f5cfce7dc2a7a3bc8
xroobix <109462535+xroobix@users.noreply.github.com>
2023-04-17 17:06:43 +0200

    Add animated counter component

--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -1,28 +1,36 @@
+import CountUp from 'react-countup';
 import './counter.css';
 
 const counterData = [
   {
-    number: "10k+",
+    number: 10,
     text: 'Happy clients',
   },
   {
-    number: "350+",
+    number: 350,
     text: 'Running projects',
   },
   {
-    number: "1000+",
+    number: 1000,
     text: 'Projects Completed',
   },
 ];
 
 export const Counter = () => {
   return (
-    <section className="counter" id='projects'>
+    <section className="counter" id="projects">
       <div className="container">
         <div className="counter__wrapper">
           {counterData.map((i, idx) => (
             <div key={idx} className="counter__item">
-              <h3 className="counter__number">{i.number}</h3>
+              <h3 className="counter__number">
+                <CountUp
+                  end={i.number}
+                  suffix={i.number > 100 ? `+` : `K+`}
+                  separator=""
+                  enableScrollSpy={true}
+                />
+              </h3>
               <h4 className="counter__title">{i.text}</h4>
             </div>
           ))}
